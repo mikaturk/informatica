2aef927c41e94aaa69f159992512d0efa1b7151e
mikat <mika2000.10@gmail.com>
2016-09-29 15:31:23 +0200

    ~dbconnect

--- web/dbconnect.php
+++ web/dbconnect.php
@@ -1,6 +1,7 @@
 <?php
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["DATABASE_URL"]));
+  echo $user, $host;
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 

f87eb8f9fee1be61ceee62ba77fdf8d407ae6845
mikat <mika2000.10@gmail.com>
2016-09-29 14:07:57 +0200

    +Links

--- web/index.php
+++ web/index.php
@@ -1,1 +1,6 @@
-<?php phpinfo(); ?>
+<h1>Links</h1>
+<li>
+  <ul><a href="overzicht.php">Alle bestanden</a>
+  <ul><a href="toevoegen.php">Bestand toevoegen</a>
+  <ul><a href="zoeken.php">Bestanden zoeken</a>
+</li>
